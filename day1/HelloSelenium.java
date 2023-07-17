fc1cf590110b3ba253f8346ca1e93b4f5425bec6
Srividyashravan <139783404+Srividyashravan@users.noreply.github.com>
2023-07-17 19:51:28 +0530

    Add files via upload
    day1

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.out.println("Welcome to TestLeaf");
+		int price=200000;
+		string studentName= "SHRAVAN";
+		
+		System.out.println("The Name of the Student :"+studentName);
+	}
+
+}
